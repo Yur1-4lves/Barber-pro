99286b7da8407b948a7b233e06cdac84274f113d
gaabrielslv <gabrielso2020@gmail.com>
2026-06-18 13:10:18 -0300

    gatters e setters class Usuario
    inicialização gatters e setters, sem condições

--- src/SISTEMA/Usuario.java
+++ src/SISTEMA/Usuario.java
@@ -12,4 +12,33 @@ public abstract class Usuario {
     this.setEmail (email);
     this.setTelefone (telefone);
     this.setSenha (senha);
+  }
+  
+  public int getId (){
+    return id;
+  }
+  public void setId(int id){
+    this.id = id;
+  }
+  public String getNome (){
+    return nome;
+  }
+  public void setNome (String nome){
+
+  }
+  public String getEmail (){
+    return email;
+  }
+  public void setEmail (String email){
+  }
+  public String getTelefone (){
+    return telefone;
+  }
+  public void setTelefone (String telefone){
+  }
+  public String getSenha (){
+    return senha;
+  }
+  public void setSenha (String senha){
+  }
 }
