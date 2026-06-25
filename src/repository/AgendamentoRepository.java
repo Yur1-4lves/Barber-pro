7725974c8b058785c4289a663fb3f67f6bc83ab8
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-25 09:59:44 -0300

    Implement Agendamento repository methods

--- src/repository/AgendamentoRepository.java
+++ src/repository/AgendamentoRepository.java
@@ -1,4 +1,46 @@
 package repository;
 
+import SISTEMA.Agendamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgendamentoRepository {
+
+    private List<Agendamento> agendamentos;
+
+    public AgendamentoRepository() {
+        agendamentos = new ArrayList<>();
+    }
+
+    public void salvar(Agendamento agendamento) {
+        agendamentos.add(agendamento);
+    }
+
+    public void cancelar(int id) {
+
+        Agendamento agendamento =
+                buscarPorId(id);
+
+        if (agendamento != null) {
+            agendamento.cancelarAgendamento();
+        }
+    }
+
+    public List<Agendamento> listarAgendamentos() {
+        return agendamentos;
+    }
+
+    public Agendamento buscarPorId(int id) {
+
+        for (Agendamento agendamento :
+                agendamentos) {
+
+            if (agendamento.getId() == id) {
+                return agendamento;
+            }
+        }
+
+        return null;
+    }
 }
