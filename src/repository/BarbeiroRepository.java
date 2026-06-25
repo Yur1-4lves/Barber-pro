d3de22617bf9d03c312dfad968956c95b2f225d8
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-25 10:00:38 -0300

    Add BarbeiroRepository with CRUD operations
    Implement methods to save, find, list, and remove 'Barbeiro' entities.

--- src/repository/BarbeiroRepository.java
+++ src/repository/BarbeiroRepository.java
@@ -1,4 +1,56 @@
 package repository;
 
+import SISTEMA.Barbeiro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BarbeiroRepository {
+
+    private List<Barbeiro> barbeiros;
+
+    public BarbeiroRepository() {
+        barbeiros = new ArrayList<>();
+    }
+
+    public void salvar(Barbeiro barbeiro) {
+
+        for (Barbeiro b : barbeiros) {
+
+            if (b.getEmail().equalsIgnoreCase(
+                    barbeiro.getEmail())) {
+
+                throw new RuntimeException(
+                        "E-mail já cadastrado."
+                );
+            }
+        }
+
+        barbeiros.add(barbeiro);
+    }
+
+    public Barbeiro buscarPorId(int id) {
+
+        for (Barbeiro barbeiro : barbeiros) {
+
+            if (barbeiro.getId() == id) {
+                return barbeiro;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Barbeiro> listarTodos() {
+        return barbeiros;
+    }
+
+    public void remover(int id) {
+
+        Barbeiro barbeiro = buscarPorId(id);
+
+        if (barbeiro != null) {
+            barbeiros.remove(barbeiro);
+        }
+    }
 }
