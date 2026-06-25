136dd3c3534b4d9a92fb358a378083936c9cee05
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-25 10:02:07 -0300

    Implement PagamentoRepository for payment management

--- src/repository/PagamentoRepository.java
+++ src/repository/PagamentoRepository.java
@@ -1,4 +1,36 @@
 package repository;
 
+import SISTEMA.Pagamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PagamentoRepository {
+
+    private List<Pagamento> pagamentos;
+
+    public PagamentoRepository() {
+        pagamentos = new ArrayList<>();
+    }
+
+    public void salvar(Pagamento pagamento) {
+        pagamentos.add(pagamento);
+    }
+
+    public Pagamento buscarPagamento(int id) {
+
+        for (Pagamento pagamento :
+                pagamentos) {
+
+            if (pagamento.getId() == id) {
+                return pagamento;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Pagamento> listarPagamentos() {
+        return pagamentos;
+    }
 }
