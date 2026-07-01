2e519cad55ba2d8edf0869a55ff633b9e43c5305
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-30 22:13:46 -0300

    Refactor AgendamentoController package and methods

--- src/controller/AgendamentoController.java
+++ src/controller/AgendamentoController.java
@@ -1,4 +1,30 @@
-package controller;
+package CONTROLLERS;
+
+import REPOSITORIES.AgendamentoRepository;
+import SERVICES.AgendamentoService;
+import SISTEMA.Agendamento;
+
+import java.util.List;
 
 public class AgendamentoController {
+
+    private AgendamentoService service;
+    private AgendamentoRepository repository;
+
+    public AgendamentoController(AgendamentoService service, AgendamentoRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    public void criarAgendamento(Agendamento agendamento) {
+        service.criarAgendamento(agendamento);
+    }
+
+    public void cancelarAgendamento(int id) {
+        service.cancelarAgendamento(id);
+    }
+
+    public List<Agendamento> listarAgendamentos() {
+        return repository.listarAgendamentos();
+    }
 }
