fc4f9ed3827af69b41fa936712607e255c48110f
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-30 22:30:17 -0300

    Add formatting methods for time and phone numbers
    Added methods to format time and phone numbers.

--- src/utils/Formatador.java
+++ src/utils/Formatador.java
@@ -1,4 +1,16 @@
 package utils;
 
 public class Formatador {
+
+    public static String formatarHorario(String horario) {
+        return horario;
+    }
+
+    public static String formatarTelefone(String telefone) {
+        if (telefone.length() == 11) {
+            return "(" + telefone.substring(0, 2) + ") " + telefone.substring(2, 7) + "-" + telefone.substring(7);
+        }
+
+        return telefone;
+    }
 }
