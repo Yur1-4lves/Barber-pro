26ecb06b1e85d8d2414d2fe2087b5f2476c43ca9
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-30 22:14:37 -0300

    Refactor ClienteController with repository methods

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,4 +1,49 @@
-package controller;
+package CONTROLLERS;
+
+import REPOSITORIES.ClienteRepository;
+import SISTEMA.Cliente;
 
 public class ClienteController {
+
+    private ClienteRepository repository;
+
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        repository.salvar(cliente);
+    }
+
+    public boolean loginCliente(
+            String email,
+            String senha) {
+
+        Cliente cliente =
+                repository.buscarPorEmail(email);
+
+        if (cliente == null) {
+            return false;
+        }
+
+        return cliente.login(email, senha);
+    }
+
+    public void editarPerfil(
+            int id,
+            String nome,
+            String email,
+            String telefone) {
+
+        Cliente cliente =
+                repository.buscarPorId(id);
+
+        if (cliente != null) {
+
+            cliente.atualizarDados(
+                    nome,
+                    email,
+                    telefone);
+        }
+    }
 }
