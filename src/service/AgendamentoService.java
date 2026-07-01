a35a6483c180380c9254873a7ff1db9b087062a0
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-30 22:23:23 -0300

    Refactor AgendamentoService with scheduling methods

--- src/service/AgendamentoService.java
+++ src/service/AgendamentoService.java
@@ -1,4 +1,66 @@
-package service;
+package SERVICES;
+
+import EXCEPTIONS.HorarioIndisponivelException;
+import REPOSITORIES.AgendamentoRepository;
+import SISTEMA.Agenda;
+import SISTEMA.Agendamento;
+import SISTEMA.Barbeiro;
 
 public class AgendamentoService {
+
+    private AgendamentoRepository repository;
+
+    public AgendamentoService(AgendamentoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void criarAgendamento(Agendamento agendamento) {
+        if (!oBarbeiroTrabalhaNesseHorario(agendamento)) {
+            throw new HorarioIndisponivelException("Horário indisponível.");
+        }
+
+        Barbeiro barbeiro = agendamento.getBarbeiro();
+
+        Agenda agenda = barbeiro.getAgenda();
+
+        for (Agendamento a : agenda.getAgendamentos()) {
+
+            boolean mesmaData = a.getHorario().equals(agendamento.getHorario());
+
+            if (mesmaData) {
+                throw new HorarioIndisponivelException("Já existe um agendamento neste horário.");
+            }
+        }
+
+        repository.salvar(agendamento);
+    }
+
+    public void cancelarAgendamento(int id) {
+
+        Agendamento agendamento = repository.buscarPorId(id);
+
+        if (agendamento != null) {
+
+            agendamento.cancelarAgendamento();
+
+            agendamento.getBarbeiro()
+                    .getAgenda()
+                    .adicionarHorario(
+                            agendamento.getHorario());
+
+        }
+
+    }
+
+    public boolean oBarbeiroTrabalhaNesseHorario(Agendamento agendamento) {
+        return oBarbeiroTrabalhaNesseHorario(
+                agendamento.getBarbeiro(),
+                agendamento.getHorario()
+        );
+    }
+
+    public boolean oBarbeiroTrabalhaNesseHorario(Barbeiro barbeiro, String horario) {
+        Agenda agenda = barbeiro.getAgenda();
+        return agenda.verificarDisponibilidade(horario);
+    }
 }
