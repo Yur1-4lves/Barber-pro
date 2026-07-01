7a784398ee4cfb27f568bd35d26818d8ebf58058
Yuri A. Araújo <araujo07@alu.ufc.br>
2026-06-30 22:14:12 -0300

    Update BarbeiroController.java

--- src/controller/BarbeiroController.java
+++ src/controller/BarbeiroController.java
@@ -1,4 +1,41 @@
-package controller;
+package CONTROLLERS;
+
+import REPOSITORIES.BarbeiroRepository;
+import SISTEMA.Barbeiro;
+
+import java.util.List;
 
 public class BarbeiroController {
+
+    private BarbeiroRepository repository;
+
+    public BarbeiroController(
+            BarbeiroRepository repository) {
+
+        this.repository = repository;
+    }
+
+    public void cadastrarBarbeiro(
+            Barbeiro barbeiro) {
+
+        repository.salvar(barbeiro);
+    }
+
+    public List<Barbeiro> listarBarbeiros() {
+        return repository.listarTodos();
+    }
+
+    public void atualizarDisponibilidade(
+            int id,
+            boolean disponibilidade) {
+
+        Barbeiro barbeiro =
+                repository.buscarPorId(id);
+
+        if (barbeiro != null) {
+
+            barbeiro.atualizarDisponibilidade(
+                    disponibilidade);
+        }
+    }
 }
